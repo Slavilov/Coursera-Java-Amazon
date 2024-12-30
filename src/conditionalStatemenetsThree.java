77fc4b3d259c33ec51be85167f753f2a1db32635
Velichko Slavilov <velichkoslavilov98@gmail.com>
2024-12-30 18:44:39 +0200

    Added 2 more exercises!

---
+++ src/conditionalStatemenetsThree.java
@@ -0,0 +1,16 @@
+public class conditionalStatemenetsThree
+{
+    public static void main(String[] args)
+    {
+
+        String testString = "This year Toby was a good boy!";
+        if (testString.contains(testString))
+        {
+            System.out.println("You get a gift from Santa!");
+        }
+        else
+        {
+            System.out.println("Next year, please!");
+        }
+    }
+}
