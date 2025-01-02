4f7b88ffa3b8fd687b034a2627cf49e4beb9bba8
Velichko Slavilov <velichkoslavilov98@gmail.com>
2025-01-02 18:17:55 +0200

    Add files via upload

---
+++ Exercises/src/conditionalStatementsFour.java
@@ -0,0 +1,17 @@
+public class conditionalStatementsFour
+{
+    public static void main(String[] args)
+    {
+
+        String fullNameOne = "Johnny Lee";
+        String fullNameTwo = "Tobby Lee";
+        if (fullNameOne.equalsIgnoreCase(fullNameTwo))
+        {
+            System.out.println("They are the same!");
+        }
+        else
+        {
+            System.out.println("They are not the same!");
+        }
+    }
+}
