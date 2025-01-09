d72e25d7ed46459add7e4ae3f796362beb523b7d
Velichko Slavilov <velichkoslavilov98@gmail.com>
2025-01-10 00:11:54 +0200

    Add files via upload

---
+++ Exercises/src/FavoriteBook.java
@@ -0,0 +1,15 @@
+public class FavoriteBook {
+    public static void main(String args[])
+    {
+        String title = "Brothers Karamazov";
+        String author = "Fyodor Dostoevsky";
+        int numberOfPages = 955;
+        double priceOfBook = 28.45;
+
+        System.out.println("Title of the book: " + title);
+        System.out.println("Name of the Author: " + author);
+        System.out.println("Number of pages of the book: " + numberOfPages);
+        System.out.println("The price of the book is: " + priceOfBook);
+
+    }
+}
