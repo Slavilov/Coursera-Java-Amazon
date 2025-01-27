13953c85615d67f0b4dc85108415f0a1364d6a96
Velichko Slavilov <velichkoslavilov98@gmail.com>
2025-01-27 12:33:55 +0200

    Update Main.java

--- implementing-multiple-inheritance-interfaces/src/Main.java
+++ implementing-multiple-inheritance-interfaces/src/Main.java
@@ -3,22 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Object of SideKick class
         SideKick sideKickObject = new SideKick();
-
-
-        // Object of WatADriver class
         WatADriver driverBot = new WatADriver();
-
-        // Object of Scanner class to get input from the user
         Scanner keyboardInput = new Scanner(System.in);
-
-        // integer variable to get choice of user as to which robot to use
         int menuChoice;
-
-        // boolean variable to keep the do-while running
         boolean keepMenuRunning;
-
 
         do {
 
@@ -64,27 +53,20 @@ public class Main {
 
             }
 
-
             // in case the input skips
             keyboardInput.nextLine();
 
-            // prompt user if continuation is required for loop
             System.out.println("Another run? (enter y for yes/ any value for no):  ");
-            //get input
             String userInput = keyboardInput.nextLine();
 
-            // check if input is equal to "y"
             if(userInput.equalsIgnoreCase("y")) {
-                // re-run loop
                 keepMenuRunning = true;
             } else {
-                // stop loop
                 keepMenuRunning = false;
             }
 
         } while (keepMenuRunning);
     }
-
 
     /** TODO 3: Create an interface named "KungFuSkills" **/
 
@@ -93,8 +75,5 @@ public class Main {
      *          to declare a method named "kungFuProtection()".
      *          Ensure that the  return type of the method is "void".
      **/
-
-
-
 
 }
