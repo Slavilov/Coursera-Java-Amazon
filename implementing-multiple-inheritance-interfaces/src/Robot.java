96dbdc1e6c70e42c2b4ef520cccb3a4f811f1fa9
Velichko Slavilov <velichkoslavilov98@gmail.com>
2025-01-27 12:35:35 +0200

    Update Robot.java

--- implementing-multiple-inheritance-interfaces/src/Robot.java
+++ implementing-multiple-inheritance-interfaces/src/Robot.java
@@ -1,27 +1,16 @@
 abstract public class Robot {
 
-    /** mode of operation  **/
     private int modeOfOperation;
-
-    /** charge of battery  **/
     private int batteryCharge;
 
-    /** default constructor **/
     public Robot() {
-        // initial charge of battery is 0
         batteryCharge = 0 ;
-
-        // set default to cleaning or 1
         modeOfOperation = 1;
     }
 
-
-    // accessor/getter method  for modeOfOperation
     public int getModeOfOperation() {
         return modeOfOperation;
     }
-
-    // mutator/setter method for modeOfOperation
     public void setModeOfOperation(int modeOfOperation) {
         if(modeOfOperation>=1 && modeOfOperation<=4) {
             this.modeOfOperation = modeOfOperation;
@@ -30,17 +19,12 @@ abstract public class Robot {
         }
     }
 
-
-    /* Getter method */
     public int getBatteryCharge() {
         return batteryCharge;
     }
-
-    /* Setter method */
     public void setBatteryCharge(int batteryCharge) {
         this.batteryCharge = batteryCharge;
     }
-
 
     public void rechargeBattery() {
         System.out.println("Plug into socket.....");
@@ -55,18 +39,12 @@ abstract public class Robot {
 
     }
 
-
     public void displayBatteryLevel() {
         System.out.println("The battery charge is at :" + batteryCharge + " %.");
     }
 
-
     abstract public void setChoice();
-
-    // method to take action
     abstract public void takeAction();
-
-
 }
 
 
